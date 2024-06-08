1396f332675bde0878c5bf9f260c4c4878b2ac06
guest1 <guest1@ima.domain>
2024-06-08 13:59:11 +0530

    server done

---
+++ server/routes/routes.js
@@ -0,0 +1,25 @@
+import express from 'express'
+import { Register, Login, Auth } from '../controller/userController.js'
+const router = express.Router()
+import { body } from 'express-validator'
+import { VerifyUser } from '../middleware/VerifyUser.js'
+
+
+router.post('/register',[
+    body('name').trim().notEmpty().withMessage("Name is Required"),
+    body('email').trim().notEmpty().withMessage("Email ID is Required")
+    .isEmail().withMessage("Email Invalid"),
+    body('password').trim().notEmpty().withMessage("Password is Required")
+    .isLength({min: 5, max: 30}).withMessage("Password Length must Be 3-50 Characters")
+], Register )
+
+router.post('/login',[
+    body('email').trim().notEmpty().withMessage("Email ID is Required")
+    .isEmail().withMessage("Email Invalid"),
+    body('password').trim().notEmpty().withMessage("Password is Required")
+    .isLength({min: 5, max: 30}).withMessage("Password Length must Be 3-50 Characters")
+], Login )
+
+router.get('/verify', VerifyUser,Auth )
+
+export { router as Router };
