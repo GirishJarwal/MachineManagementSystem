4cc7d0b271fb386d89ea35ab732d681f4ad85eef
guest1 <guest1@ima.domain>
2024-06-12 15:10:14 +0530

    getching details done

---
+++ server/controller/machineController.js
@@ -0,0 +1,71 @@
+import express from 'express'
+import { MachineModel } from '../models/Machine.js'
+
+const createMachine = async (req,res) => {
+    const{name,customer, city, model, ccnstatus, iolist,
+        vfdparameters, sensorlist,historysheet, alarmlist, electricaldrawing,
+        hmimanual, accesslevel,parameterrange, cfrchecklist,
+        temperatureparameters,motorcommreport, servolicense,
+        zenonlicense, docsmartupload, basicwiring, energizingdate,
+        optionalwiring, optionaltesting, stickers, prefatchecklist,
+        ifatdate, eeifatpoints, ifatclosuredate, cfatdate, eecfatpoints,
+        cfatclosure, eenonissuance,finalphoto,  finaldocument, finalprogram,
+        dispatchdate, dispatchmonth, installation, updatemachine,
+        docrelatedcomplaints, logicrelatedcomplaints, details
+    } = req.body;
+    
+    try{
+    const newMachine = new MachineModel({
+        name, //ccn
+        customer,
+        city,
+        model,
+        ccnstatus,
+        iolist,
+        vfdparameters,
+        sensorlist,
+        historysheet,
+        alarmlist,
+        electricaldrawing,
+        hmimanual,
+        accesslevel,
+        parameterrange,
+        cfrchecklist,
+        temperatureparameters,
+        motorcommreport,
+        servolicense,
+        zenonlicense,
+        docsmartupload,
+        basicwiring,
+        energizingdate,
+        optionalwiring,
+        optionaltesting,
+        stickers,
+        prefatchecklist,
+        ifatdate,
+        eeifatpoints,
+        ifatclosuredate,
+        cfatdate,
+        eecfatpoints,
+        cfatclosure,
+        eenonissuance,
+        finalphoto,
+        finaldocument,
+        finalprogram,
+        dispatchdate,
+        dispatchmonth,
+        installation,
+        updatemachine,
+        docrelatedcomplaints,
+        logicrelatedcomplaints,
+        details
+    })
+
+    const result = await newMachine.save()
+    return res.status(201).json({success:true, ...result._doc})
+}catch(err){
+    return res.status(500).json(err.message);
+}
+};
+
+export {createMachine}
---
+++ server/models/Machine.js
@@ -0,0 +1,142 @@
+import mongoose, { model } from "mongoose";
+
+const MachineSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        require: true,
+        unique: true
+    },
+    customer: {
+        type: String,
+        require: true,
+    },
+    city: {
+        type: String,
+        require: true
+    },
+    model:{
+        type: String,
+        require: true
+    },
+    ccnstatus:{
+        type: String,
+        require: true
+    },
+    iolist:{
+        type: String,
+    },
+    vfdparameters:{
+        type: String,
+    },
+    sensorlist:{
+        type: String,
+    },
+    historysheet:{
+        type: String,
+    },
+    alarmlist:{
+        type: String,
+    },
+    electricaldrawing:{
+        type: String,
+    },
+    hmimanual:{
+        type: String,
+    },
+    accesslevel:{
+        type: String,
+    },
+    parameterrange:{
+        type: String,
+    },
+    cfrchecklist:{
+        type: String,
+    },
+    temperatureparameters:{
+        type: String,
+    },
+    motorcommreport:{
+        type: String,
+    },
+    servolicense:{
+        type: String,
+    },
+    zenonlicense:{
+        type: String,
+    },
+    docsmartupload:{
+        type: String,
+    },
+    basicwiring:{
+        type: String,
+    },
+    energizingdate:{
+        type: String,
+    },
+    optionalwiring:{
+        type: String,
+    },
+    optionaltesting:{
+        type: String,
+    },
+    stickers:{
+        type: String,
+    },
+    prefatchecklist:{
+        type: String,
+    },
+    ifatdate:{
+        type: String,
+    },
+    eeifatpoints:{
+        type: String,
+    },
+    ifatclosuredate:{
+        type: String,
+    },
+    cfatdate:{
+        type: String,
+    },
+    eecfatpoints:{
+        type: String,
+    },
+    cfatclosure:{
+        type: String,
+    },
+    eenonissuance:{
+        type: String,
+    },
+    finalphoto:{
+        type: String,
+    },
+    finaldocument:{
+        type: String,
+    },
+    finalprogram:{
+        type: String,
+    },
+    dispatchdate:{
+        type: String,
+    },
+    dispatchmonth:{
+        type: String,
+    },
+    installation:{
+        type: String,
+    },
+    updatemachine:{
+        type: String,
+    },
+    docrelatedcomplaints:{
+        type: String,
+    },
+    logicrelatedcomplaints:{
+        type: String,
+    },
+    details:{
+        type: String,
+    }
+})
+
+const MachineModel = mongoose.model("machine", MachineSchema)
+export {MachineModel}
